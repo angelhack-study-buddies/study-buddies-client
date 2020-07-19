9515f675797cef8d5574b20a7f2c5b0d08061caf
daclouds <daclouds@gmail.com>
2020-07-19 16:53:54 +0900

    Update like

--- src/components/recommendations.tsx
+++ src/components/recommendations.tsx
@@ -1,37 +1,31 @@
 import React, { Fragment } from 'react'
-import { RouteComponentProps, navigate } from '@reach/router'
+import { RouteComponentProps } from '@reach/router'
 
-import { Col, Card, Icon, CardTitle, Button  } from 'react-materialize'
+import { Col } from 'react-materialize'
 import './list.css'
 import { Post } from '../generated/graphql'
+import PostCard from './post-card'
 
 interface RecommendationsProps extends RouteComponentProps {
   items: [Post]
 }
 
 const Recommendations: React.FC<RecommendationsProps> = (props) => {
+  if (!props.items?.length) {
+    return (
+      <div style={{ verticalAlign: 'middle'}}>
+        There is no item to recommend now.<br /><br />
+        Press the profile image and post one by clicking the floating button on the bottom right.
+      </div>
+    )
+  }
   return (
     <dl className="recommendations">
       <h5>Recommendations</h5>
       {props.items?.map(item => (
         <Fragment key={item.id}>
           <Col m={6}>
-            <Card 
-              actions={[
-                <Button key={item.id} onClick={() => navigate(item.url!)}>Go To</Button>,
-              ]}
-              header={
-                <Fragment>
-                  <CardTitle image={item.previewImage!} />
-                  <div className="icons">
-                    <span onClick={() => navigate(`/profile/${item.author?.id}`)}><Icon>account_circle</Icon></span>
-                    <span onClick={() => alert('like!')}><Icon>favorite_border</Icon></span>
-                  </div>
-                </Fragment>
-              }
-            >
-              <p>{item.title}</p>
-            </Card>
+            <PostCard item={item} />
           </Col>
         </Fragment>
       ))}
